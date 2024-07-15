bb6dacd271ae2bb6ba968d7c18e41c51cef1901d
potatoj1n <appabomul@gmail.com>
2024-07-15 10:08:55 +0900

    feat : 헤더 확인을 위해 메인 페이지 생성

--- src/routers/Routers.tsx
+++ src/routers/Routers.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Layout } from './Layout';
 import Login from '../pages/Login/Login';
+import Main from '../pages/Main/Main';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    children: [{ path: '/', element: <Login /> }],
+    children: [
+      { path: '/', element: <Login /> },
+      { path: '/main', element: <Main /> },
+    ],
   },
 ]);
 

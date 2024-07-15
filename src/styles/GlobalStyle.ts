b7a31a8ef22dcb3b5050390a588d01f34bdf1429
potatoj1n <appabomul@gmail.com>
2024-07-15 10:09:22 +0900

    Design : 글로벌 스타일 컴포넌트 생성

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const TitleText = styled.h1``;
+
+export const subTitleText = styled.h2`
+  font-size: 28px;
+  font-weight: 700;
+`;
+export const PlainText = styled.p`
+  font-size: 24px;
+  font-weight: 500;
+`;
+export const LinkText = styled.p`
+  font-size: 20px;
+  color: #a9b5bc;
+`;
+export const noticeText = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  color: #ff7417;
+`;

c350bfc6c2178ab596fa49458d261d07bf9a5708
potatoj1n <appabomul@gmail.com>
2024-07-16 10:47:43 +0900

    Design: 메인 페이지 퍼블리싱

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { BandWrapper, MainContainer, MainWrapper, TitleWrapper } from './MainStyle';
+import { LinkText, SubTitleText } from '../../styles/GlobalStyle';
+import Banner from '../../components/main/Banner';
 
 export default function Main() {
-  return <div></div>;
+  return (
+    <MainWrapper>
+      <MainContainer>
+        <Banner />
+        <TitleWrapper>
+          <SubTitleText>모집 중인 밴드</SubTitleText>
+          <LinkText>더 알아보기</LinkText>
+        </TitleWrapper>
+        <BandWrapper></BandWrapper>
+        <TitleWrapper>
+          <SubTitleText>주변 공연장 & 합주실을 찾아보세요</SubTitleText>
+          <LinkText>나의 장소 등록하기</LinkText>
+        </TitleWrapper>
+      </MainContainer>
+    </MainWrapper>
+  );
 }

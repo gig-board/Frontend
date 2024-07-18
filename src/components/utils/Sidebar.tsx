bddc5932c6520c5cf95cbdacc5dc79ca6b8695d3
potatoj1n <appabomul@gmail.com>
2024-07-18 11:30:57 +0900

    feat : 밴드 모집 라우터 연결

--- src/components/utils/Sidebar.tsx
+++ src/components/utils/Sidebar.tsx
@@ -43,7 +43,7 @@ const MenuItems = styled.p`
 `;
 const MenuItem = [
   { text: '공연 홍보', path: '/map' },
-  { text: '밴드 모집', path: '/map' },
+  { text: '밴드 모집', path: '/recruitband' },
   { text: '나의 팀짜기', path: '/map' },
   { text: '연습실 찾기', path: '/map' },
   { text: 'MYPAGE', path: '/mypage' },

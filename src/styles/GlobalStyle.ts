4370d7abced45d6a5675dc76d8d87081be22ab5b
potatoj1n <appabomul@gmail.com>
2024-07-18 10:49:10 +0900

    Design : 사진 백그라운드 필터 컴포넌트 생성

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+//텍스트
 export const TitleText = styled.h1`
   font-size: 48px;
   font-weight: 700;
@@ -38,6 +39,7 @@ export const EditText = styled.p`
   cursor: pointer;
 `;
 
+//인풋 스타일
 export const ShortInput = styled.input`
   width: 100%;
   height: 1vh;
@@ -46,10 +48,24 @@ export const MultiInput = styled.input`
   width: 100%;
   height: 10vh;
 `;
+
+//버튼 스타일
 export const SubmmitButton = styled.button`
   border: none;
   border-radius: 10px;
   background-color: #ff9045;
   cursor: pointer;
   padding: 10px 20px;
+`;
+
+//오버레이 스타일
+export const PictureOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background-color: rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(5px);
 `;

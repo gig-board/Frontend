28e319ed532cb040e49f07694ca5444882407f86
potatoj1n <appabomul@gmail.com>
2024-07-20 09:41:58 +0900

    Design : 마진 속성 글로벌 스타일로 적용

--- src/pages/Main/MainStyle.ts
+++ src/pages/Main/MainStyle.ts
@@ -1,12 +1,5 @@
 import { styled } from 'styled-components';
 
-export const MainWrapper = styled.div`
-  width: 100%;
-`;
-export const MainContainer = styled.div`
-  margin: 2rem 10%;
-`;
-
 export const BandWrapper = styled.div`
   width: 100%;
   height: auto;

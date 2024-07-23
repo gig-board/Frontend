2f20e0287dcfbd06dd1945e1426277166de7e6b3
potatoj1n <appabomul@gmail.com>
2024-07-23 22:00:19 +0900

    feat : 로그인 토큰 세션에 저장

--- src/pages/Login/KakaoAuth.tsx
+++ src/pages/Login/KakaoAuth.tsx
@@ -10,12 +10,13 @@ export default function KakaoAuth() {
     if (code) {
       const FetchData = async () => {
         try {
-          const response = await axios.get(`http://배포주소:8080/api/kakao/callback?code=${code}`);
-
+          const response = await axios.post(`http://배포주소:8080/v1/auth/login/kakao?code=${code}`);
           console.log(response.data);
-          // 사용자 정보를 로컬 스토리지에 저장하거나 상태 관리
+
           const ACCESS_TOKEN = response.data.accessToken;
-          localStorage.setItem('token', ACCESS_TOKEN);
+          const REFRESH_TOKEN = response.data.refreshToken;
+          localStorage.setItem('accessToken', ACCESS_TOKEN);
+          localStorage.setItem('refreshToken', REFRESH_TOKEN);
           navigate('/main');
         } catch (error) {
           console.error('카카오 로그인 실패', error);

dbbcc4d68c5969867932e0d7f93c258f55b318f6
potatoj1n <appabomul@gmail.com>
2024-07-23 10:51:45 +0900

    feat : 모집글 생성 캘린더 라이브러리,컴포넌트 추가

---
+++ src/components/utils/Calendar.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import '../../styles/React-Calendar.css';
+import { Value } from 'react-calendar/dist/cjs/shared/types';
+
+function App() {
+  // 상태의 타입을 Date, [Date, Date], 또는 null로 설정
+  const [date, setDate] = useState<Date | [Date, Date] | null>(null);
+
+  // 날짜 선택 핸들러
+  const handleDateChange = (value: Value) => {
+    setDate(value as Date | [Date, Date] | null);
+  };
+
+  return (
+    <>
+      {Array.isArray(date) ? (
+        <p className="display: flex justify-around">
+          <span className="bold">시작일 : {date[0].toDateString()}</span>
+          <span className="bold">종료일 : {date[1].toDateString()}</span>
+        </p>
+      ) : date ? (
+        <p className="text-center">
+          <span className="bold">Default selected date:</span> {date.toDateString()}
+        </p>
+      ) : (
+        <p className="text-center">
+          <span className="bold">No date selected</span>
+        </p>
+      )}
+      <div className="calendar-container">
+        <Calendar onChange={handleDateChange} value={date} selectRange={true} />
+      </div>
+    </>
+  );
+}
+
+export default App;

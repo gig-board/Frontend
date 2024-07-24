899ed52121a03dcb30b22eeed5ec6d86d90d8686
potatoj1n <appabomul@gmail.com>
2024-07-24 18:50:53 +0900

    feat : 밴드 모집 상세보기 페이지 퍼블리싱

--- src/pages/RecruitBand/RecruitStyle.ts
+++ src/pages/RecruitBand/RecruitStyle.ts
@@ -23,3 +23,51 @@ export const AllRecruitContainer = styled.div`
   gap: 2vw;
   margin-top: 2vw;
 `;
+//모집 상세보기
+
+export const DetailContainer = styled.div`
+  margin: 0 10%;
+`;
+
+export const BandImgContainer = styled.div<{ backgroundImageUrl: string }>`
+  width: 100%;
+  height: 40vw;
+  background-image: url(${props => props.backgroundImageUrl});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+`;
+
+export const BandTitleContainer = styled.div`
+  width: 100%;
+  height: 55%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem;
+`;
+export const BandDetailContainer = styled.div`
+  width: 100%;
+  height: 45%;
+  border: none;
+  background-color: rgba(255, 255, 255, 0.6);
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  gap: 1rem;
+`;
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+export const BandImg = styled.div<{ backgroundImageUrl: string }>`
+  width: 13rem;
+  height: 13rem;
+  background-image: url(${props => props.backgroundImageUrl});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+`;

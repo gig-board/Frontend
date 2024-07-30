4f21d193157ccbcdcb92f1daf62103abfecf298d
potatoj1n <appabomul@gmail.com>
2024-07-30 14:42:52 +0900

    feat : 동아리원 등록하기 페이지 퍼블리싱

--- src/pages/Club/ClubBoard.tsx
+++ src/pages/Club/ClubBoard.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { DetailText, GlobalWrapper, ShortInput, SmallText } from '../../styles/GlobalStyle';
+import { AddButton, AddMemberContainer, ClubWrapper, MemberContainer } from './ClubStyle';
 
-function ClubBoard() {
-  return <div></div>;
+export default function ClubBoard() {
+  return (
+    <GlobalWrapper>
+      <ClubWrapper>
+        <AddMemberContainer>
+          <DetailText>동아리명</DetailText>
+          <DetailText>동아리원을 추가해주세요.</DetailText>
+          <SmallText>이름을 입력하세요.</SmallText>
+          <ShortInput />
+          <SmallText>세션을 선택하세요.</SmallText>
+          <SmallText>동아리원의 수준을 선택하세요.</SmallText>
+          <AddButton>멤버 등록하기</AddButton>
+        </AddMemberContainer>
+        <MemberContainer>
+          <DetailText>동아리원을 원하는 팀으로 드래그해주세요.</DetailText>
+        </MemberContainer>
+      </ClubWrapper>
+    </GlobalWrapper>
+  );
 }
-
-export default ClubBoard;

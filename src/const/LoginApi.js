ea0576df575c49fa2f1e1a6feab3cb573bb26d9b
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-10-24 18:07:12 +0900

    Update LoginApi.js

--- src/const/LoginApi.js
+++ src/const/LoginApi.js
@@ -1,4 +1,4 @@
 const REST_API_KEY = 'ae11385695952793834fa6be545ea908';
-const REDIRECT_URI = 'http://localhost:3000/api/kakao/callback';
+const REDIRECT_URI = 'http://localhost:5000/api/kakao/callback';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
